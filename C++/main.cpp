4924abede1d905ab6ed907150f0629ee0b0c78cc
kkkksdx <401126398@qq.com>
2025-07-05 12:05:25 +0800

    studying C++ code

--- C++/main.cpp
+++ C++/main.cpp
@@ -5,6 +5,47 @@
 // 顶点缓冲区、着色器
 // opengl是一个状态机
 
+//  编译着色器
+static unsigned int CompileShader(unsigned int type, const std::string &source)
+{
+    unsigned int id = glCreateShader(type);
+    const char *src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    //  TODE Error handing
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char *message = (char *)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cerr << "Failed to compile shader!" << std::endl;
+        std::cerr << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+// 创建着色器
+// vertexShader 是顶点着色器的代码, fragmentShader 是片段着色器的代码
+static unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    //  删除着色器, 因为已经链接到程序中
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow *window;
@@ -12,8 +53,6 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
-    glewInit();
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -24,12 +63,20 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    glfwSwapInterval(1);
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        return -1;
+    }
+    unsigned char *glVersion = 0;
+    // GLCall(glVersion = (unsigned char*)glGetString(GL_VERSION));
     /* Loop until the user closes the window */
-    float points[] = {
-        0.0f, 0.0f, 0.0f, // Vertex 1 (X, Y, Z)
-        1.0f, 0.0f, 0.0f, // Vertex 2 (X, Y, Z)
-        0.5f, 1.0f, 0.0f  // Vertex 3 (X, Y, Z)
+    float points[6] = {
+        0.0f, 0.0f, // Vertex 1 (X, Y, Z)
+        1.0f, 0.0f, // Vertex 2 (X, Y, Z)
+        0.5f, 1.0f  // Vertex 3 (X, Y, Z)
     };
     //  声明顶点缓冲区
     unsigned int buffer;
@@ -42,8 +89,30 @@ int main(void)
     //  points是一个数组，包含了三个顶点的坐标
     //  每个顶点有两个坐标（X, Y）
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), points, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);                                          // 启用顶点属性数组
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // 设置顶点属性指针
+
     glBindBuffer(GL_ARRAY_BUFFER, 0); // 解绑缓冲区
-    //  声明着色器
+                                      //  声明着色器
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = position;\n"
+        "}\n";
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    color = vec4(1.0, 0.0, 0.0, 1.0);\n" // 设置颜色为红色
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader); // 使用着色器程序
 
     while (!glfwWindowShouldClose(window))
     {
@@ -61,7 +130,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
